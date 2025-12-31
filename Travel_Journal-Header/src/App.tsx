08620f709703d873a289438113f4040443df266a
kvn2004 <knimsaravihanga@gmail.com>
2026-01-01 05:11:26 +0530

    entry coponent re-usable with props

--- Travel_Journal-Header/src/App.tsx
+++ Travel_Journal-Header/src/App.tsx
@@ -6,7 +6,18 @@ function App() {
   return (
     <>
       <Header />
-      <Entry />
+      <Entry 
+      location='JAPAN' 
+      placeName='Mount Fuji' 
+      dates='12 Jan, 2021 - 24 Jan, 2021' 
+      description='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,389 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.' mainImageSrc='/src/images/mount-fuji.jpg' 
+      googleMapsLink='https://www.google.com/maps/place/Mount+Fuji/@35.3606255,138.7273634,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634' />
+      <Entry 
+      location='JAPAN' 
+      placeName='Mount Fuji' 
+      dates='12 Jan, 2021 - 24 Jan, 2021' 
+      description='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,389 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.' mainImageSrc='/src/images/mount-fuji.jpg' 
+      googleMapsLink='https://www.google.com/maps/place/Mount+Fuji/@35.3606255,138.7273634,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634' />
     </>
   )
 }
--- Travel_Journal-Header/src/Components/Entry.tsx
+++ Travel_Journal-Header/src/Components/Entry.tsx
@@ -1,22 +1,30 @@
 import React from 'react'
 
-function Entry() {
+interface EnttriesProps {
+    location: string;
+    placeName: string;
+    dates: string;
+    description: string;
+    mainImageSrc: string;
+    googleMapsLink: string;
+}
+
+function Entry({location, placeName, dates, description, mainImageSrc, googleMapsLink}: EnttriesProps) {
   return (
     <article className='journal-entry'>
         <div className='main-img-container'>
-            <img src="/src/images/mount-fuji.jpg" alt="" className='main-img' />
+            <img src={mainImageSrc} alt="" className='main-img' />
         </div>
         <div>
             <div>
                 <img src="/src/images/marker.png" alt="Location Pin" className='marker-img'/>
-                <span className='location'>JAPAN</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606255,138.7273634,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634" target="_blank" rel="noopener noreferrer">View on Google Maps</a>
+                <span className='location'>{location}</span>
+                <a href={googleMapsLink} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
             </div>
             <div>
-                <h2 className='place-name'>Mount Fuji</h2>
-                <p className='dates'>Dates: 12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className='des'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,389 feet). 
-                Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h2 className='place-name'>{placeName}</h2>
+                <p className='dates'>Dates: {dates}</p>
+                <p className='des'>{description}</p>
             </div>
         </div>
     </article>
